b251be762ddfa0411e4a6ca86696acf3d8050933
Rachel Cartwright <stormchica@gmail.com>
2014-12-04 23:00:32 -0700

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140429185809) do
+ActiveRecord::Schema.define(version: 20141205055319) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -29,5 +29,13 @@ ActiveRecord::Schema.define(version: 20140429185809) do
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+
+  create_table "users", force: true do |t|
+    t.integer  "user_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
